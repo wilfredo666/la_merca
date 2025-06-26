ff689f2e90b154d34a995ab549e3d3290e57d124
wilfredo666 <31371350+wilfredo666@users.noreply.github.com>
2025-06-25 21:41:42 -0400

    062525

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,15 +1,30 @@
 <?php
 
 class Conexion{
-  
+
   static public function conectar(){
-    
-    $host="localhost";
+
+    /* =============================
+         PARA TRABAJAR DE MANERA LOCAL 
+         =================================*/
+
+/*    $host="localhost";
     $db="la_merca";
     $userDB="root";
     $passDB="";
-    
-    $link=new PDO("mysql:host=".$host.";"."dbname=".$db, $userDB, $passDB);
+
+    $link=new PDO("mysql:host=".$host.";"."dbname=".$db, $userDB, $passDB);*/
+
+    /* =============================
+      PARA TRABAJAR CON EL SERVIDOR REMOTO
+      =================================*/
+
+    $host = "localhost";
+    $db = "u144048036_la_merca";
+    $userDB = "u144048036_root";
+    $passDB = "Lamerca123!";
+    $link = new PDO("mysql:host=" . $host . ";" . "dbname=" . $db, $userDB, $passDB);
+
     $link->exec("set names utf8");
     return $link;
   }
